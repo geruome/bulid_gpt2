55fbd805e8149aae6602413678eb8836971f679a
geruome <2665633561@qq.com>
2024-11-06 17:43:54 +0800

    [2401213398] add from_pretrained and test_model

--- model.py
+++ model.py
@@ -94,7 +94,7 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # bias=False ???
 
-    def forward(self, idx): # idx is the tokenized result, (B,T)
+    def forward(self, idx, targets=None): # idx is the tokenized result, (B,T)
         B, T = idx.size()
         assert T <= self.config.block_size, f"Can't forward sequence of length {T}, block size is only {self.config.block_size}"
 
@@ -108,3 +108,87 @@ class GPT(nn.Module):
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x) # B, T, vocab_size
         return logits
+    
+    @classmethod
+    def from_pretrained(cls, model_type):
+        "Loads pretrained GPT-2 model from huggingface transformers"
+        assert model_type in ['gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl']
+        print(f"loading weights from pretrained gpt: {model_type}")
+
+        config_args = {
+            'gpt2': dict(n_layer=12, n_head=12, n_embd=768),  # 124M
+            'gpt2-medium': dict(n_layer=24, n_head=16, n_embd=1024), # 350M
+            'gpt2-large':  dict(n_layer=36, n_head=20, n_embd=1280), # 774M
+            'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600), # 1558M
+        }[model_type]
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias') and not k.endswith('.attn.bias')] # ignore the buffer params
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']\
+        # nn.Linear(x,y): output = input * weight^T + bias, so shape of weight is (y,x)
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # print(k, sd_hf[k].shape[::-1], sd[k].shape)
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = GPT.from_pretrained('gpt2')
+# model = GPT(GPTConfig())
+model.to(device)
+model.eval()
+
+tokenizer = tiktoken.get_encoding('gpt2')
+
+def test_model():
+    B = 5
+    max_length = 30
+
+    tokenizer = tiktoken.get_encoding('gpt2')
+    input_sentence = "Hello, I'm a language model,"
+    x = tokenizer.encode(input_sentence)
+    x = torch.tensor(x, dtype=torch.long, device=device)
+    x = x.unsqueeze(0).repeat(B, 1) # B, T
+
+    set_seed(42)
+    while x.shape[1] < max_length:
+        with torch.no_grad():
+            logits = model(x) # B, T, vocab_size
+            logits = logits[:, -1, :]  # B, vocab_size
+            probs = F.softmax(logits, dim=-1)  # logits != probs , softmax is needed
+            # 50 is default in huggingface pipeline.  Clamp down rare probs.
+            topk_probs, topk_ids = torch.topk(probs, 50, dim=-1) # (B, 50). 
+            ix = torch.multinomial(topk_probs, 1) # sampling
+            nx = torch.gather(topk_ids, -1, ix) # get data a/to dim and idx
+            x = torch.cat((x, nx), dim=1) # B, T+1
+
+    lst = x.tolist()
+    print(input_sentence)
+    for idx in lst:
+        print(">", tokenizer.decode(idx))
+
+test_model()
